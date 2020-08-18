23fa23de04fd5889c75d0d5f07f2d35639068862
Tomas Giro Larraz <girotomas@gmail.com>
2020-08-18 19:34:49 +0200

    I added the logo

---
+++ test_flexbox/logo.js
@@ -0,0 +1,5 @@
+var div = document.getElementById('logoDiv');
+div.innerHTML = `
+	<link href="https://fonts.googleapis.com/css2?family=Libre+Caslon+Display&display=swap" rel="stylesheet">
+	<p style='font-family:"Libre Caslon Display";font-size:30px;margin:3px;position:absolute;left: 50%;transform: translateX(-70px)'>MERMAID<span style='font-size:12px;position:relative;top:-10px'>&trade;</span></p>
+`;
